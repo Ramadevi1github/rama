fb7734c46731c525ce7cecdafb789ff86888a07f
Ramadevi1github <119603968+Ramadevi1github@users.noreply.github.com>
2022-12-01 08:51:44 -0800

    Add files via upload

---
+++ Zomato.java
@@ -0,0 +1,48 @@
+package com.restaurant.com.restaurant.zomato;
+
+public class Zomato {
+	private String restaurantname;
+	private int restaurantId;
+	private String deliverymode;
+	private int averagePrice;
+	private int rating;
+	private String location;
+
+	public String getRestaurantname() {
+		return restaurantname;
+	}
+	public void setRestaurantname(String restaurantname) {
+		this.restaurantname = restaurantname;
+	}
+	public int getRestaurantId() {
+		return restaurantId;
+	}
+	public void setRestaurantId(int restaurantId) {
+		this.restaurantId = restaurantId;
+	}
+	public String getDeliverymode() {
+		return deliverymode;
+	}
+	public void setDeliverymode(String deliverymode) {
+		this.deliverymode = deliverymode;
+	}
+	public int getAveragePrice() {
+		return averagePrice;
+	}
+	public void setAveragePrice(int averagePrice) {
+		this.averagePrice = averagePrice;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	
+}
